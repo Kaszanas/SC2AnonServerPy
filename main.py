16d4f3b0841f353fba6353436bc3e3f9c0af3cb5
Andrzej Białecki <kaszanas@interia.pl>
2020-08-21 19:19:09 +0200

    Removed old replays, added replays from scientific dataset

--- main.py
+++ main.py
@@ -17,10 +17,20 @@ def format_replay(replay):
 
     # return output
 
+from datetime import datetime
+loop_start_time = datetime.now()
 replays = sc2reader.load_replays('./DEMOS/Input', load_level=4)
-for replay in replays:
+for index, replay in enumerate(replays):
+
+    replay_start_time = datetime.now()
 
     name_of_replay = os.path.splitext(os.path.basename(replay.filename))[0]
     with open(f'./DEMOS/Output/{name_of_replay}.pickle', 'wb') as f:
         pickle.dump(replay, f)
     print(format_replay(replay))
+
+    replay_end_time = datetime.now() - replay_start_time
+    print(replay_end_time)
+
+loop_end_time = datetime.now() - loop_start_time
+print(loop_end_time)
