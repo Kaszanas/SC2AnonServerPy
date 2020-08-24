f91a75d2fdef45b94c394127e3544d4d8fc07422
Andrzej Białecki <kaszanas@interia.pl>
2020-08-24 12:05:02 +0200

    Tested anonymization

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import sc2reader
 from PACAnalyzer.pacanalyzer import PACAnalyzer
 import pickle
+import random
+
 
 import os
 
 sc2reader.engine.register_plugin(PACAnalyzer())
+
+
+def anonymize(replay):
+    # Delete player
+    replay.client[0].name = random.randint(0, 100)
+    replay.client[0].toon_handle = None
+    replay.client[0].toon_id = None
+
+    return replay
+
 
 
 def format_replay(replay):
@@ -25,8 +37,9 @@ for index, replay in enumerate(replays):
     replay_start_time = datetime.now()
 
     name_of_replay = os.path.splitext(os.path.basename(replay.filename))[0]
+    replay = anonymize(replay)
     with open(f'./DEMOS/Output/{name_of_replay}.pickle', 'wb') as f:
-        pickle.dump(replay, f)
+        pickle.dump(anonymize(replay), f)
     print(format_replay(replay))
 
     replay_end_time = datetime.now() - replay_start_time
@@ -34,3 +47,5 @@ for index, replay in enumerate(replays):
 
 loop_end_time = datetime.now() - loop_start_time
 print(loop_end_time)
+
+
