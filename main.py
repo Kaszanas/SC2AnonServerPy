f9c3b9283b64f099bc5d280c5d1ff53d15606be0
Andrzej Białecki <kaszanas@interia.pl>
2020-08-26 18:09:53 +0200

    Defined draft of a client

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
+import os
+
+# gRPC imports:
+import grpc
+import anonymize_pb2
+import anonymize_pb2_grpc
+
+# Replay processing imports:
 import sc2reader
 from PACAnalyzer.pacanalyzer import PACAnalyzer
 import pickle
-import random
 
-
-import os
 
 sc2reader.engine.register_plugin(PACAnalyzer())
 
-
 def anonymize_nicknames(replay):
+    nicknames = [replay.client[0], replay.client[1]] # TODO: Fix this line for the exact object
+
+    with grpc.insecure_channel("localhost:9999") as channel:
+        stub = anonymize_pb2_grpc.AnonymizeServiceStub(channel)
+
+        for index, nickname in enumerate(nicknames):
+            response = stub.getAnonymizedID(anonymize_pb2.SendNickname(nickname=nickname))
+            replay.client[index] = response # TODO: Fix this line for the exact object
+
     return replay
 
 def anonymize(replay):
@@ -21,35 +34,12 @@ def anonymize(replay):
 
     return replay
 
+def process_replays(replays_directory):
+    loop_start_time = datetime.now()
+    replays = sc2reader.load_replays(f'{replays_directory}', load_level=4)
+    for index, replay in enumerate(replays):
 
-
-def format_replay(replay):
-    pass
-    # output = replay.name
-    # for team in replay.teams:
-    #     for player in team.players:
-    #         if player.is_human:
-                # output += str(player.PACStats.ppm)
-
-    # return output
-
-from datetime import datetime
-loop_start_time = datetime.now()
-replays = sc2reader.load_replays('./DEMOS/Input', load_level=4)
-for index, replay in enumerate(replays):
-
-    replay_start_time = datetime.now()
-
-    name_of_replay = os.path.splitext(os.path.basename(replay.filename))[0]
-    replay = anonymize(replay)
-    with open(f'./DEMOS/Output/{name_of_replay}.pickle', 'wb') as f:
-        pickle.dump(anonymize(replay), f)
-    print(format_replay(replay))
-
-    replay_end_time = datetime.now() - replay_start_time
-    print(replay_end_time)
-
-loop_end_time = datetime.now() - loop_start_time
-print(loop_end_time)
-
-
+        name_of_replay = os.path.splitext(os.path.basename(replay.filename))[0]
+        replay = anonymize(replay)
+        with open(f'./DEMOS/Output/{name_of_replay}.pickle', 'wb') as f:
+            pickle.dump(anonymize(replay), f)
