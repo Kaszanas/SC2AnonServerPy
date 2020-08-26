8fb15166f63a16ddf42927c33d49e4289cd39f6e
Andrzej Białecki <kaszanas@interia.pl>
2020-08-26 18:09:53 +0200

    Defined draft of a client

---
+++ deprecated.py
@@ -0,0 +1,9 @@
+def format_replay(replay):
+    pass
+    # output = replay.name
+    # for team in replay.teams:
+    #     for player in team.players:
+    #         if player.is_human:
+                # output += str(player.PACStats.ppm)
+
+    # return output
