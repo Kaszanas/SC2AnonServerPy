6414325df5c946d3438cb7c9507fa373dddc8b7d
Andrzej Białecki <kaszanas@interia.pl>
2020-08-27 18:01:01 +0200

    Different multiprocessing approach

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from client import process_replays, anonymize, anonymize_nicknames
-import multiprocessing as mp
+from client import process_replay, anonymize, anonymize_nicknames
+from multiprocessing import Pool
 import argparse
 import os
+import logging
+from itertools import product
 
 # Initiate multiprocessing spawning processes that are using load_replay
 # This must be done by popping a list so that processes don't have the same replay by accident.
@@ -16,11 +18,17 @@ def get_replays(replay_directory:str):
     return absolute_filepaths
 
 
-def start_processing(replay_directory:str):
+def start_processing(replay_directory:str, output_directory:str):
 
     list_of_replays = get_replays(replay_directory)
 
-    process_jobs = []
-    while process_jobs < 24:
-        while list_of_replays:
-            p = mp.Process(target=process_replays, args=(list_of_replays.pop(),))
+    # process_jobs = []
+    # while process_jobs < 24:
+    #     while list_of_replays:
+    #         p = mp.Process(target=process_replays, args=(list_of_replays.pop(),))
+
+    agents = 23
+    chunksize = 20
+    with Pool(processes=agents) as pool:
+        replay = list_of_replays.pop()
+        pool.map(process_replay, product(replay, output_directory), chunksize)
