f6c7a4480a905ef4f7f8249d49db21b44469b651
Andrzej Białecki <kaszanas@interia.pl>
2020-08-30 01:02:41 +0200

    Added settings file, added logging

---
+++ settings.py
@@ -0,0 +1,1 @@
+LOGGING_FORMAT = "[%(asctime)s][%(name)s][%(module)s][%(levelname)s] - %(message)s"
--- test.py
+++
@@ -1,6 +0,0 @@
-import pickle
-
-with open("./DEMOS/Output/11506437_1566325283_230842.pickle", "rb") as loaded_pickle:
-    unpacked = pickle.load(loaded_pickle)
-    
-    print("sdsd")
