965e4fc198fb13ae95f1071d7de37932b5a41b50
Andrzej Białecki <kaszanas@interia.pl>
2020-08-29 03:40:59 +0200

    Renamed files, created methods for loading hashed nicknames

--- grpc_server.py
+++ grpc_server.py
@@ -11,13 +11,23 @@ class Listener(anonymize_pb2_grpc.AnonymizeServiceServicer):
     def SendNickname(self, request, context):
         logging.info(f"Received nickname = {request.nickname}")
 
-        # TODO: Check if the nickname is currently in a saved .pickle file
+        with open("./anonymized_player_database.pickle") as anonymized_db:
 
-        # If the nickname is not in the file add it
+            if not request.nickname in anonymized_db:
 
-        # Add the nickname as the key and check for highest current value of generated ID and add new key/value pair with highest_id += 1
-        logging.info(f"Mapped nickname = {request.nickname} to ID = {}")
+            # TODO: Check if the nickname is currently in a saved .pickle file
+
+            # If the nickname is not in the file add it
+
+            # Add the nickname as the key and check for highest current value of generated ID and add new key/value pair with highest_id += 1
+            logging.info(f"Mapped nickname = {request.nickname} to ID = {}")
         return anonymize_pb2.ReceiveID(anonymized_id=1)
+
+    def load_data():
+        pass
+
+    def save_data():
+        pass
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
